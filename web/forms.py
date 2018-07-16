d8043f958ea6ddf3e4bf91ea86770e61acab5712
wenguonideshou <153329152@qq.com>
2018-07-17 01:11:51 +0800

    Delete forms.py

--- web/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class SearchForm(forms.Form):
-    keyword = forms.CharField(label="关键字", max_length=50, widget=forms.TextInput(
-        attrs={'id': 'search-input', 'class': 'search-input', 'tabIndex': '1', 'autocomplete': "off"}))
